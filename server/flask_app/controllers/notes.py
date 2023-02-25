78d1b5f47f999551f1ab7e58586d6140a2ba716c
Kyle Hench <kylehench@gmail.com>
2023-02-24 17:33:53 -0800

    renamed note controller

--- server/flask_app/controllers/notes.py
+++ server/flask_app/controllers/notes.py
@@ -1,27 +1,69 @@
 from flask_app import app, db
-from flask_app.models.note import Note, NoteSchema
-from flask import request
-# from login_required_dec import login_required_dec
+from flask_restful import reqparse, abort, Api, Resource, request
+import flask_app.models.note
+from flask_app.models.note import Note, note_schema
 from flask_app.services.authorization import user_id_from_token
 
-@app.route('/api/test1', methods=['POST'])
-# @login_required_dec
-def create_note():
+api = Api(app)
 
-  note = Note(title='To Do', content='Go to the grocery store')
-  db.session.add(note)
-  db.session.commit()
+parser = reqparse.RequestParser()
+parser.add_argument('title')
+parser.add_argument('content')
 
-  note = Note.query.first()
-  note_schema = NoteSchema()
-  output = note_schema.dump(note)
-  print(output)
+def get_user_id():
+  try:
+    user_id = user_id_from_token(request)
+    return user_id
+  except:
+    abort(401, message="Please sign in.")
+
+class NoteResource(Resource):
+  def get(self, note_id):
+    user_id = get_user_id()
+    note = db.session.execute(db.select(Note)
+      .where(Note.id==note_id)
+    ).scalar()
+    # check permission to view note
+    if note.user_id != user_id:
+      abort(401, message="You do not have authorization to view this resource.")
+    return {'note': note_schema.dump(note)}
   
-  return {'note': output}
+  def put(self, note_id):
+    user_id = get_user_id()
+    new_note_dict = parser.parse_args()
+    note = db.session.scalar(db.select(Note).where(Note.id == note_id))
+    # check permission to update note
+    if user_id != note.user_id:
+      abort(401, message="You do not have authorization to edit this resource.")
+    note.title = new_note_dict['title']
+    note.content = new_note_dict['content']
+    db.session.commit()
+    return '', 204
+  
+  def delete(self, note_id):
+    user_id = get_user_id()
+    note = db.session.get(Note, note_id)
+    if not note or note.user_id != user_id:
+      abort(401, message="Invalid request.")
+    db.session.delete(note)
+    db.session.commit()
+    return '', '204'
 
-@app.route('/api/test2')
-def test2():
-  print(request.cookies.get('usertoken'))
-  user_id = user_id_from_token(request)
-  print(user_id)
-  return f'{user_id}'
+
+
+class NoteListResource(Resource):
+  def get(self):
+    notes = db.session.execute(db.select(Note.id, Note.title).order_by(Note.updated_at.desc()))
+    notes = [{"id": id, "title": title} for id, title in notes]
+    return notes
+  
+  def post(self):
+    user_id = get_user_id()
+    note = Note(user_id=user_id, **parser.parse_args())
+    db.session.add(note)
+    db.session.commit()
+    return note.id, 201
+  
+# setup API resource routing
+api.add_resource(NoteResource, '/api/notes/<note_id>')
+api.add_resource(NoteListResource, '/api/notes')
