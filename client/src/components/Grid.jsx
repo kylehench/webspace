f8089fa7587edbea04b8988a09ddb5a873f4c47c
Kyle Hench <kylehench@gmail.com>
2023-03-14 17:51:45 -0700

    setTimeout test

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -19,7 +19,7 @@ const Grid = ({ appState }) => {
   setTimeout(() => {
     // prevents initial layout state ([]) overwriting layout and widgets in localstorage
     setGridActive(true)
-  }, 100);
+  }, 2000);
 
   // initial load layout
   useEffect(() => {
