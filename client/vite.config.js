7ca30b6f96d3d0b5c502f09c25e059e56e120414
Kyle Hench <kylehench@gmail.com>
2023-05-17 23:03:12 -0700

    added warning for client missing .env file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/webspace',
-  server: {
-    proxy: {
-      '/auth': {
-        target: 'http://localhost:4000/',
-        rewrite: (path) => path.replace(/^\/auth/, '')
-      },
-      '/webspace/api': {
-        target: 'http://localhost:5000/',
-        rewrite: (path) => path.replace(/^\/webspace/, '')
+export default ({mode}) => {
+  // Path to the .env file
+  const envFilePath = path.resolve(__dirname, '.env')
+  
+  // Check if the .env file exists
+  if (!fs.existsSync(envFilePath)) {
+    throw new Error('.env file not found. Please create an .env file. See .env.example for reference.');
+  }
+
+  return defineConfig({
+    plugins: [react()],
+    base: '/webspace',
+    server: {
+      proxy: {
+        '/auth': {
+          target: 'http://localhost:4000/',
+          rewrite: (path) => path.replace(/^\/auth/, '')
+        },
+        '/webspace/api': {
+          target: 'http://localhost:5000/',
+          rewrite: (path) => path.replace(/^\/webspace/, '')
+        }
       }
     }
-  }
-})
+  })
+}
