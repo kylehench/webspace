fd324fc7eef0e2db604c1443614af7dde1fdfb59
Kyle Hench <kylehench@gmail.com>
2023-05-26 22:40:11 -0700

    corrected comment

--- client/src/components/LoginRegButton.jsx
+++ client/src/components/LoginRegButton.jsx
@@ -7,12 +7,11 @@ import axios from 'axios'
 const LoginRegButton = ({ appState }) => {
   const [open, setOpen] = useState(false)
 
-  // request token (jwt) renewal if expiration present in localStorage
+  // request token (jwt) renewal if token present and within 21 days of expiration
   useEffect(() => {
     const token_expires = localStorage.getItem('webspace_token_expires')
     if (token_expires) {
       const expires = new Date(token_expires)
-      // request token renewal if within 21 days of expiration
       if (expires.getTime() < new Date().getTime() + 21*24*60*60*1000) {
         axios.post(`${import.meta.env.VITE_AUTH_URI}/api/renew-token`)
           .then(res => {
