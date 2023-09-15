e15ae6bf92c7c865ceb723b28694e4fde61dc776
Kyle Hench <kylehench@gmail.com>
2023-09-14 17:22:58 -0700

    weather location saved to local storage

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import axios from 'axios'
 import GridItem from './primitives/GridItem'
 import COUNTRY_CODES from '../config/countryCodes'
@@ -14,8 +14,9 @@ const Weather = ({ appState, widgetProps }) => {
 
   const [ countryInput, setCountryInput ] = useState('')
   const [ countryResults, setCountryResults ] = useState([])
-  const [ countryCode, setCountryCode ] = useState('')
-  const [ zipCode, setZipCode ] = useState('')
+
+  const [ countryCode, setCountryCode ] = useState(widgetProps.countryCode)
+  const [ zipCode, setZipCode ] = useState(widgetProps.zipCode)
 
   const [ weatherData, setWeatherData ] = useState()
   const [ temperatureMax, setTemperatureMax ] = useState(0)
@@ -79,8 +80,15 @@ const Weather = ({ appState, widgetProps }) => {
       setGraphSlope(graphSlope)
       setGraphIntercept(-tMax*graphSlope)
       setWeatherDataGenerated(new Date())
+      widgetsDispatch({type: "UPDATE", reactId: widgetProps.reactId, payload: {countryCode, zipCode}})
     })
   }
+
+  useEffect(() => {
+    if (widgetProps.countryCode && widgetProps.zipCode) {
+      weatherRequest()
+    }
+  }, [])
 
   return (
     <GridItem
@@ -91,6 +99,8 @@ const Weather = ({ appState, widgetProps }) => {
     >
       <div className='py-2 pl-2 h-full thin-scrollbar-parent'>
         { weatherData ?
+
+          // display weather if weatherData is present
           <div className='h-full flex justify-between items-stretch divide-x divide-slate-300'>
             { weatherData.map((day) => {
               const barY = graphSlope*day.high + graphIntercept
@@ -138,64 +148,71 @@ const Weather = ({ appState, widgetProps }) => {
               )
             })}
           </div>
-        :
-          <div className='h-full px-3 overflow-auto thin-scrollbar'>
+        : <>
+          {(!widgetProps.zipCode && !widgetProps.countryCode) ?
 
-            {/* country search box */}
-            <fieldset className="mb-[10px] flex flex-col justify-start">
-              <label className="text-[13px] leading-none mb-2.5 text-cyan12 block">
-                Enter Country
-              </label>
-              <input
-                className={`grow shrink-0 rounded px-2.5 text-[15px] leading-none text-cyan11 shadow-[0_0_0_1px] shadow-cyan7 h-[33px] focus:shadow-[0_0_0_2px] focus:shadow-cyan8 outline-none  ${countryCode ? 'bg-green-100' : 'bg-white'}`}
-                type="text"
-                value={countryInput}
-                onChange={handleCountryInput}
-              />
-            </fieldset>
+            // display form if zipCode and countryCode is unknown, otherwise display loading symbol
+            <div className='h-full px-3 overflow-auto thin-scrollbar'>
 
-            {/* country list picker */}
-            <div className='absolute border rounded bg-white'>
-              { countryResults.map(({code, name}, i) =>
-                <div
-                  key={code}
-                  className='w-60 border-cyan11 p-2.5 text-[15px] leading-none text-cyan11 border-1 shadow-cyan7 cursor-pointer truncate hover:bg-slate-200'
-                  onClick={() => {
-                    setCountryCode(code)
-                    setCountryInput(name)
-                    setCountryResults([])
-                  }}
-                >
-                  {name}
+                {/* country search box */}
+                <fieldset className="mb-[10px] flex flex-col justify-start">
+                  <label className="text-[13px] leading-none mb-2.5 text-cyan12 block">
+                    Enter Country
+                  </label>
+                  <input
+                    className={`grow shrink-0 rounded px-2.5 text-[15px] leading-none text-cyan11 shadow-[0_0_0_1px] shadow-cyan7 h-[33px] focus:shadow-[0_0_0_2px] focus:shadow-cyan8 outline-none  ${countryCode ? 'bg-green-100' : 'bg-white'}`}
+                    type="text"
+                    value={countryInput}
+                    onChange={handleCountryInput}
+                  />
+                </fieldset>
+
+                {/* country list picker */}
+                <div className='absolute border rounded bg-white'>
+                  { countryResults.map(({code, name}, i) =>
+                    <div
+                      key={code}
+                      className='w-60 border-cyan11 p-2.5 text-[15px] leading-none text-cyan11 border-1 shadow-cyan7 cursor-pointer truncate hover:bg-slate-200'
+                      onClick={() => {
+                        setCountryCode(code)
+                        setCountryInput(name)
+                        setCountryResults([])
+                      }}
+                    >
+                      {name}
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
 
-            {/* zip code input */}
-            <fieldset className="mb-[10px] w-full flex flex-col justify-start">
-              <label className="text-[13px] leading-none mb-2.5 text-cyan12 block">
-                Enter Zip Code
-              </label>
-              <input
-                className='grow shrink-0 rounded px-2.5 text-[15px] leading-none text-cyan11 shadow-[0_0_0_1px] shadow-cyan7 h-[33px] focus:shadow-[0_0_0_2px] focus:shadow-cyan8 outline-none bg-white'
-                type="text"
-                value={zipCode}
-                onChange={e => setZipCode(e.target.value)}
-              />
-            </fieldset>
+                {/* zip code input */}
+                <fieldset className="mb-[10px] w-full flex flex-col justify-start">
+                  <label className="text-[13px] leading-none mb-2.5 text-cyan12 block">
+                    Enter Zip Code
+                  </label>
+                  <input
+                    className='grow shrink-0 rounded px-2.5 text-[15px] leading-none text-cyan11 shadow-[0_0_0_1px] shadow-cyan7 h-[33px] focus:shadow-[0_0_0_2px] focus:shadow-cyan8 outline-none bg-white'
+                    type="text"
+                    value={zipCode}
+                    onChange={e => setZipCode(e.target.value)}
+                  />
+                </fieldset>
 
-            {/* set location button */}
-            <div className="mt-3 flex justify-center">
-              <button
-                className="rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-slate-200 text-slate-500 hover:bg-slate-300 focus:shadow-[0_0_0_2px] focus:shadow-slate-400 outline-none cursor-default"
-                onClick={weatherRequest}
-              >
-              Set Location
-            </button>
-        </div>
+                {/* set location button */}
+                <div className="mt-3 flex justify-center">
+                  <button
+                    className="rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-slate-200 text-slate-500 hover:bg-slate-300 focus:shadow-[0_0_0_2px] focus:shadow-slate-400 outline-none cursor-default"
+                    onClick={weatherRequest}
+                  >
+                    Set Location
+                  </button>
+                </div>
 
-          </div>
-        }
+              </div>
+            :
+            <div>Loading...</div>
+          }
+
+        </>}
       </div>
     </GridItem>
   )
